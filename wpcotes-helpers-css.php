4c776dca5ba3bce1ee6731409f8b9c07a8d83662
Willy Cotes <willycotes@gmail.com>
2022-05-16 02:06:50 -0500

    save changes

--- wpcotes-helpers-css.php
+++ wpcotes-helpers-css.php
@@ -7,6 +7,6 @@ add_action( 'wp_enqueue_scripts', 'wpcotes_helpers_css', 10, 1 );
 
 if ( ! function_exists( 'wpcotes_helpers_css') ) {
 	function wpcotes_helpers_css() {
-		wp_enqueue_style( 'wpcotes-helpers', __DIR__ . '/css/helpers.css', array(), true, 'all' );
+		wp_enqueue_style( 'wpcotes-helpers', __DIR__ . '/css/helpers.css', array(), true, '1.0.0' );
 	}
 }
