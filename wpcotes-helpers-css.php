d30a2257b1b3fd431146d01220d5593fbda49cd2
Willy Cotes <willycotes@gmail.com>
2022-05-16 02:37:46 -0500

    fixed function

--- wpcotes-helpers-css.php
+++ wpcotes-helpers-css.php
@@ -3,10 +3,10 @@
  * Plugin Name: WPCotes - Helpers CSS
  */
 
-add_action( 'wp_enqueue_scripts', 'wpcotes_helpers_css', -10, 1 );
+add_action( 'wp_enqueue_scripts', 'wpcotes_helpers_css', 10, 1 );
 
 if ( ! function_exists( 'wpcotes_helpers_css') ) {
 	function wpcotes_helpers_css() {
-		wp_enqueue_style( 'wpcotes-helpers', __DIR__ . '/css/helpers.css', array(), true, '1.0.0' );
+		wp_enqueue_style( 'wpcotes-helpers', __DIR__ . '/css/helpers.css', array(), '1.0', 'all' );
 	}
 }
