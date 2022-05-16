2203b3d066893a9ecea5311b175be19d6be1b346
Willy Cotes <willycotes@gmail.com>
2022-05-16 02:04:12 -0500

    first commit

---
+++ wpcotes-helpers-css.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: WPCotes - Helpers CSS
+ */
+
+add_action( 'wp_enqueue_scripts', 'wpcotes_helpers_css', 10, 1 );
+
+if ( ! function_exists( 'wpcotes_helpers_css') ) {
+	function wpcotes_helpers_css() {
+		wp_enqueue_style( 'wpcotes-helpers', __DIR__ . '/css/helpers.css', array(), true, 'all' );
+	}
+}
